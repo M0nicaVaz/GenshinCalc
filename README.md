d260dff504d166dd18c39fcf51e5b587b795eb81
Monica Vaz <contatomonicavaz@gmail.com>
2022-02-24 20:25:22 -0300

    fix footer position

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Primogem Calculator
+
 <p> Programmed to calculate the amount of Genshin Impact's primogems that can be achieved during a limited time, according to the information provided by the user. </p>
 
 ### Tech
+
 - JavaScript
 - HTML
 - CSS
 
 **NOTE**
+
 <p> This project was created for study purposes only. I do not intend to profit from it. Please do not commercialize. </p>
--- app/js/script.js
+++ app/js/script.js
@@ -1,21 +1,21 @@
-let primos = document.querySelector("#primogens");
-let Welkin = document.querySelector("#Welkin");
-let battlepass = document.querySelector("#battlepass");
-let daily = document.querySelector("#daily");
-let submitBtn = document.querySelector("#submit");
-let setDate = document.querySelector("#finalDate");
-let result = document.querySelector("#result");
-let wraper = document.querySelector("#wraper");
-let again = document.querySelector("#tryAgain");
-let showSpan = document.querySelector("#clickInfo");
+let primos = document.querySelector('#primogens');
+let Welkin = document.querySelector('#Welkin');
+let battlepass = document.querySelector('#battlepass');
+let daily = document.querySelector('#daily');
+let submitBtn = document.querySelector('#submit');
+let setDate = document.querySelector('#finalDate');
+let result = document.querySelector('#result');
+let wraper = document.querySelector('#wraper');
+let again = document.querySelector('#tryAgain');
+let showSpan = document.querySelector('#clickInfo');
 
 // change div display
-showSpan.addEventListener("click", () => {
-  let showP = document.querySelector("#info");
-  if (showP.style.display != "block") {
-    showP.style.display = "block";
+showSpan.addEventListener('click', () => {
+  let showP = document.querySelector('#info');
+  if (showP.style.display != 'block') {
+    showP.style.display = 'block';
   } else {
-    showP.style.display = "none";
+    showP.style.display = 'none';
   }
 });
 // get today's date, target date and calculates difference between dates
@@ -27,59 +27,59 @@ function getDays() {
   return daysDiff.toFixed(0);
 }
 // calculates amount of primogems according to input values
-function calcPrimogens(){
-    if (primos.value == "") {
-        currentPrimos = 0;
-      } else {
-        currentPrimos = Number(primos.value);
-      }
-      if (daily.checked == true) {
-        var dailyGems = getDays() * 60;
-      } else {
-        var dailyGems = 0;
-      }
-      if (Welkin.checked == true) {
-        var Welkinems = getDays() * 90;
-      } else {
-        var Welkinems = 0;
-      }
-      if (battlepass.checked == true) {
-        var battleGems = 1320;
-      } else {
-        var battleGems = 0;
-      }
-    
-      let totalPrimos = dailyGems + Welkinems + battleGems + currentPrimos;
-      let fates = Math.floor(totalPrimos / 160);
-    
-      result.innerHTML =
-        "<p>Primogems: " +
-        "<br>" +
-        totalPrimos +
-        "</p>" +
-        "<br>" +
-        " <p> Intertwined Fates: " +
-        "<br>" +
-        fates +
-        "</p>";
-      result.style.marginTop = "20px";
-      wraper.style.display = "none";
-      submitBtn.style.display = "none";
-      result.style.display = "block";
-      again.style.display = "block";
+function calcPrimogens() {
+  if (primos.value == '') {
+    currentPrimos = 0;
+  } else {
+    currentPrimos = Number(primos.value);
+  }
+  if (daily.checked == true) {
+    var dailyGems = getDays() * 60;
+  } else {
+    var dailyGems = 0;
+  }
+  if (Welkin.checked == true) {
+    var Welkinems = getDays() * 90;
+  } else {
+    var Welkinems = 0;
+  }
+  if (battlepass.checked == true) {
+    var battleGems = 1320;
+  } else {
+    var battleGems = 0;
+  }
+
+  let totalPrimos = dailyGems + Welkinems + battleGems + currentPrimos;
+  let fates = Math.floor(totalPrimos / 160);
+
+  result.innerHTML =
+    '<p>Primogems: ' +
+    '<br>' +
+    totalPrimos +
+    '</p>' +
+    '<br>' +
+    ' <p> Intertwined Fates: ' +
+    '<br>' +
+    fates +
+    '</p>';
+  result.style.marginTop = '20px';
+  wraper.style.display = 'none';
+  submitBtn.style.display = 'none';
+  result.style.display = 'block';
+  again.style.display = 'block';
 }
 // checks if date was filled in correctly, if so calls another function
-submitBtn.addEventListener("click", () => {
-  if (setDate.value == ""){
-    alert ("Please insert date!")
-}else{
+submitBtn.addEventListener('click', () => {
+  if (setDate.value == '') {
+    alert('Please insert date!');
+  } else {
     calcPrimogens();
-}
+  }
 });
 // change display back to initial state
-again.addEventListener("click", () => {
-  result.style.display = "none";
-  wraper.style.display = "block";
-  submitBtn.style.display = "block";
-  again.style.display = "none";
-}); 
+again.addEventListener('click', () => {
+  result.style.display = 'none';
+  wraper.style.display = 'block';
+  submitBtn.style.display = 'block';
+  again.style.display = 'none';
+});
